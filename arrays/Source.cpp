94275e568d6b6612bf4b45d2e4d2394e40f2ccb4
knowledge35 <ibomyakmc@hotmail.com>
2020-08-31 23:13:22 +0300

    Add project files.

---
+++ arrays/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[] = {1,3,4};
+	string b[4] = { "a","b","c","d" };
+	for (int i = 0; i < 4; i++)
+	{
+		cout << b[i] << endl;
+	}
+	
+	cout << a[0] << endl;
+	system("pause>nul");
+}
